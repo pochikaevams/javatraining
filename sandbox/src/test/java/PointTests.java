f5fcb1515e78bef91c9440789964869ab3d34efb
pochikaevams <himmerka@gmail.com>
2019-10-21 20:24:10 +0700

    Add Task 3

---
+++ sandbox/src/test/java/PointTests.java
@@ -0,0 +1,21 @@
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+
+    @Test
+    public void testDistance() {
+        Point p1= new Point(-1,-1);
+        Point p2= new Point(2,3);
+        Assert.assertEquals(p1.dist(p2),4.123105625617661);
+        }
+
+    @Test
+    public void testDistanceNotPass() {
+        Point p3= new Point(10,75);
+        Point p4= new Point(2,3);
+        Assert.assertEquals(p3.dist(p4),74);
+    }
+
+
+}
