e8a617f605a76300ba2bd0f8bbcf8a89c8bfaa47
pochikaevams <himmerka@gmail.com>
2019-12-09 15:25:36 +0700

    Add Task 20

--- mantis/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
+++ mantis/src/test/java/ru/stqa/pft/mantis/tests/LoginTests.java
@@ -1,5 +1,6 @@
 package ru.stqa.pft.mantis.tests;
 
+import javax.xml.rpc.ServiceException;
 import org.testng.annotations.Test;
 import ru.stqa.pft.mantis.appmanager.HttpSession;
 
@@ -10,7 +11,8 @@ import static org.testng.Assert.assertTrue;
 public class LoginTests extends TestBase {
 
     @Test
-    public void testLogin() throws IOException {
+    public void testLogin() throws IOException, ServiceException {
+        skipIfNotFixed(0000001);
         HttpSession session = app.newSession();
         assertTrue(session.login("administrator", "root"));
         assertTrue(session.isLoggedInAs("administrator"));
